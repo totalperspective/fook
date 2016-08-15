a1b83bbe69f92fd8f7ca5305dce4efe312165c16
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-08-15 16:04:50 +0100

    Updated project with name and links

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject totalperspective/fook "0.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject totalperspective/fook "0.2.1"
+  :description "Small utilities for datomic"
+  :url "http://github.com/totalperspective/fook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
