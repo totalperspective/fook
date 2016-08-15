81b32dd35f53641a92ef9b000974d942a15ba936
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-08-15 13:46:34 +0100

    Added function unification

--- src/fook/unify.clj
+++ src/fook/unify.clj
@@ -1,10 +1,30 @@
 (ns fook.unify
   (:require [fook.query :as q]
-            [clojure.core.unify :as u]))
+            [clojure.core.unify :as u]
+            [clojure.core.match :refer [match]]))
+
+(defn symbolise-fns
+  [form]
+  (clojure.walk/postwalk (fn [x]
+                           (if (list? x)
+                             (with-meta
+                               (apply symbol (take 2 (map name x)))
+                               {:form x})
+                             x))
+                         form))
 
 (defn unify-result
-  "Given a query, result and a form, unify the result with tha form."
+  "Given a query, result and a form, unify the result with that form."
   [query result form]
   (let [{:keys [find]} (q/normalise-query query)
-        bindings (map (partial zipmap find) result)]
-    (map (partial u/subst form) bindings)))
+        find (symbolise-fns find)
+        form (symbolise-fns form)]
+    (->> result
+         (map (partial zipmap find))
+         (map (partial u/subst form))
+         (clojure.walk/postwalk
+          (fn [x]
+            (if-let [form (:form (meta x))]
+              form
+              x))))))
+
