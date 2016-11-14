34503beb3da6f94e578d75fde75fb9e59707ca00
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-11-14 11:40:18 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject totalperspective/fook "0.2.6"
+(defproject totalperspective/fook "0.2.7"
   :description "Small utilities for datomic"
   :url "http://github.com/totalperspective/fook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.taoensso/truss "1.2.0"]
+  :dependencies [[com.taoensso/truss "1.3.6"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.unify "0.5.7"]]
   :profiles
-  {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+  {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                         [com.datomic/datomic-free "0.9.5390"]]}})
